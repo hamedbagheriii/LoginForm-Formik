2b238bc49587a813d0cad2e2e9c04caa6c7f2cc6
hami <hamedbagheri1386.19@gmail.com>
2024-07-23 13:31:37 +0330

    part4-setSubmitingValidate

--- src/components/Registerform.jsx
+++ src/components/Registerform.jsx
@@ -18,8 +18,13 @@ const initialValues = {
     favorits : ['']
 }
 
-const onSubmit = (values)=>{
+const onSubmit = (values , submitProps)=>{
     console.log(values);
+
+    setTimeout(() => {
+        submitProps.setSubmitting(false);
+        submitProps.resetForm(initialValues)
+    }, 3000);
 }
 
 const validationSchema = Yup.object({
@@ -40,7 +45,6 @@ const validationSchema = Yup.object({
 
 
 const Registerform = () => {
-
     
     const attrs = (targetValue,targetType)=>{
         return ({
@@ -54,76 +58,93 @@ const Registerform = () => {
 
 
     return (    
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} >
-            <div className='auth_container container-fluid d-flex justify-content-center align-items-center w-100 h-100-vh p-0'>
-                <div className="row w-100 justify-content-center align-items-center">
-                    <div className='border border-primary border-2 bg-dark text-white rounded-3 py-4 mt-3 col-11 col-md-8 col-lg-6 col-xl-4 py-2 px-3'>
-                        <Form className='row'>
-                            <h1 className='text-center'>
-                                <i className='fas fa-user-plus text-primary'></i>
-                            </h1>
-                            <div className="mb-3">
-                                <label htmlFor="name" className='form-label'>نام</label>
-                                <FastField {...attrs('name','text')} id='name' />
-                                <ErrorMessage name='name' component={PersonalError} />                       
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="email" className='form-label'>ایمیل</label>
-                                <FastField {...attrs('email','email')} id='email' />
-                                <ErrorMessage name='email' component={PersonalError} /> 
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="password" className='form-label'>رمز عبور</label>
-                                <FastField {...attrs('password','password')} id='password' />
-                                <ErrorMessage name='password' component={PersonalError} />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="password2" className='form-label'>تکرار رمز عبور</label>
-                                <FastField {...attrs('password2','password')} id='password2'  />
-                                <ErrorMessage name='password2' component={PersonalError} />                        
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="password2" className='form-label'>بیوگرافی</label>
-                                <FastField {...attrs('bio','text')} id='bio' component='textarea' />
-                                <ErrorMessage name='bio' component={PersonalError} />                        
-                            </div>
+        <Formik 
+         initialValues={initialValues} 
+         validationSchema={validationSchema} 
+         onSubmit={onSubmit} 
+         validateOnMount
+        >
+            
+            {formik=>{
+                console.log(formik);
+                return(
+                    <div className='auth_container container-fluid d-flex justify-content-center align-items-center w-100 h-100-vh p-0'>
+                        <div className="row w-100 justify-content-center align-items-center">
+                            <div className='border border-primary border-2 bg-dark text-white rounded-3 py-4 mt-3 col-11 col-md-8 col-lg-6 col-xl-4 py-2 px-3'>
+                                <Form className='row'>
+                                    <h1 className='text-center'>
+                                        <i className='fas fa-user-plus text-primary'></i>
+                                    </h1>
+                                    <div className="mb-3">
+                                        <label htmlFor="name" className='form-label'>نام</label>
+                                        <FastField {...attrs('name','text')} id='name' />
+                                        <ErrorMessage name='name' component={PersonalError} />                       
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="email" className='form-label'>ایمیل</label>
+                                        <FastField {...attrs('email','email')} id='email' />
+                                        <ErrorMessage name='email' component={PersonalError} /> 
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="password" className='form-label'>رمز عبور</label>
+                                        <FastField {...attrs('password','password')} id='password' />
+                                        <ErrorMessage name='password' component={PersonalError} />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="password2" className='form-label'>تکرار رمز عبور</label>
+                                        <FastField {...attrs('password2','password')} id='password2'  />
+                                        <ErrorMessage name='password2' component={PersonalError} />                        
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="password2" className='form-label'>بیوگرافی</label>
+                                        <FastField {...attrs('bio','text')} id='bio' component='textarea' />
+                                        <ErrorMessage name='bio' component={PersonalError} />                        
+                                    </div>
 
-                            <div className="col-6">
-                                <label htmlFor="city" className='form-label'>شهر</label>
-                                <FastField {...attrs('address.city','text')} id='city' />
-                                <ErrorMessage name='address.city' component={PersonalError} />                        
-                            </div>
-                            <div className="col-6 mb-4">
-                                <label htmlFor="postalCode" className='form-label'>کد پستی</label>
-                                <FastField {...attrs('address.postalCode','number')} id='postalCode' />
-                                <ErrorMessage name='address.postalCode' component={PersonalError} />                        
-                            </div>
+                                    <div className="col-6">
+                                        <label htmlFor="city" className='form-label'>شهر</label>
+                                        <FastField {...attrs('address.city','text')} id='city' />
+                                        <ErrorMessage name='address.city' component={PersonalError} />                        
+                                    </div>
+                                    <div className="col-6 mb-4">
+                                        <label htmlFor="postalCode" className='form-label'>کد پستی</label>
+                                        <FastField {...attrs('address.postalCode','number')} id='postalCode' />
+                                        <ErrorMessage name='address.postalCode' component={PersonalError} />                        
+                                    </div>
 
-                            <div className="col-6">
-                                <label htmlFor="mobilePhone" className='form-label'>شماره موبایل</label>
-                                <FastField {...attrs('phone[0]','number')} id='mobilePhone' />
-                                <ErrorMessage name='phone[0]' component={PersonalError} />                        
-                            </div>
-                            <div className="col-6 mb-3">
-                                <label htmlFor="telePhone" className='form-label'>تلفن ثابت</label>
-                                <FastField {...attrs('phone[1]','number')} id='telePhone' />
-                                <ErrorMessage name='phone[1]' component={PersonalError} />                        
-                            </div>
+                                    <div className="col-6">
+                                        <label htmlFor="mobilePhone" className='form-label'>شماره موبایل</label>
+                                        <FastField {...attrs('phone[0]','number')} id='mobilePhone' />
+                                        <ErrorMessage name='phone[0]' component={PersonalError} />                        
+                                    </div>
+                                    <div className="col-6 mb-3">
+                                        <label htmlFor="telePhone" className='form-label'>تلفن ثابت</label>
+                                        <FastField {...attrs('phone[1]','number')} id='telePhone' />
+                                        <ErrorMessage name='phone[1]' component={PersonalError} />                        
+                                    </div>
 
-                            <div className="mb-4">
-                                <label htmlFor="favorits" className='form-label'>علاقه مندی </label>
-                                <FieldArray {...attrs('favorits','text')} id='favorits' >
-                                    {props=><FavoriteField {...props} />}
-                                </FieldArray>               
-                            </div>
+                                    <div className="mb-4">
+                                        <label htmlFor="favorits" className='form-label'>علاقه مندی </label>
+                                        <FieldArray {...attrs('favorits','text')} id='favorits' >
+                                            {props=><FavoriteField {...props} />}
+                                        </FieldArray>               
+                                    </div>
 
-                            <div className="mb-2 d-flex ">
-                                <button className="btn btn-primary w-50 mx-auto">ارسال</button>
+                                    <div className="mb-2 d-flex ">
+                                        <button className="btn btn-primary w-50 mx-auto" disabled={!formik.isValid || formik.isSubmitting}>
+                                            {formik.isSubmitting ? 
+                                                <div className="spinner-border text-white mt-1" style={{fontSize:10,width:25,height:25}}>
+                                                    <span className="visually-hidden">Loading...</span>
+                                                </div>
+                                            : 'ثبت نام'}
+                                        </button>
+                                    </div>
+                                </Form>
                             </div>
-                        </Form>
+                        </div>
                     </div>
-                </div>
-            </div>
+                )
+            }}
         </Formik>  
     )
 }
